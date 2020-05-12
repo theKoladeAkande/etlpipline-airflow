ac66baebf08263648141c66197d0ee44a9eae765
theKoladeAkande <akandekolade@gmail.com>
2020-05-12 10:39:22 +0000

    fix: S3 error due to wrong copy sql statement
    wrong sql statement most epescailly json format issues
    causing s3 bucket errors fixed, to enable
    copy transactions from s3 to redshift.

--- plugins/operators/staging_redshift_tables.py
+++ plugins/operators/staging_redshift_tables.py
@@ -4,24 +4,16 @@ from airflow.utils.decorators import apply_defaults
 from airflow.contrib.hooks.aws_hook import AwsHook
 
 class StageTablesToRedshiftOperator(BaseOperator):
-    template_fields = ("s3_key")
-    copy_sql_time_format = """
+    copy_sql = """
         COPY {}
         FROM '{}'
         ACCESS_KEY_ID '{}'
         SECRET_ACCESS_KEY '{}'
-        FORMAT AS {}
-        TIMEFORMAT AS '{}'
+        REGION '{}'
+        BLANKSASNULL EMPTYASNULL
+        TIMEFORMAT as 'epochmillisecs'
+        FORMAT as JSON '{}'
     """
-
-    copy_sql="""
-        COPY {}
-        FROM '{}'
-        ACCESS_KEY_ID '{}'
-        SECRET_ACCESS_KEY '{}'
-        FORMAT AS {}
-    """
-
     @apply_defaults
     def  __init__(self,
                   redshift_conn_id="",
@@ -29,48 +21,45 @@ class StageTablesToRedshiftOperator(BaseOperator):
                   table="",
                   s3_bucket="",
                   s3_key="",
-                  json="auto",
-                  time_format=None,
+                  region="",
+                  file_format="",
                   *args,
                   **kwargs):
 
-            super(StageTablesToRedshiftOperator, self).__init__(*args, *kwargs)
+            super(StageTablesToRedshiftOperator, self).__init__(*args, **kwargs)
             self.table = table
             self.redshift_conn_id = redshift_conn_id
             self.s3_bucket = s3_bucket
             self.s3_key = s3_key
             self.aws_credentials_id = aws_credentials_id
-            self.json = json
-            self.time_format = time_format
+            self.region = region
+            self.file_format = file_format
+            self.execution_date = kwargs.get('execution_date')
 
     def execute(self, context):
         aws_hook = AwsHook(self.aws_credentials_id)
         credentials = aws_hook.get_credentials()
         redshift = PostgresHook(self.redshift_conn_id)
-        s3_path =  f"s3://{self.s3_bucket}/{self.s3_key}"
+        s3_path =  f"s3://{self.s3_bucket}/{self.s3_key}/"
         self.log.info('Clearing data from destination Redshift table')
         redshift.run(f"DELETE FROM {self.table}")
 
-        formatted_sql_timeformat = StageTablesToRedshiftOperator.copy_sql_time_format.format(
+        if self.execution_date:
+            year = self.execution_date.year
+            month = self.execution_date.month
+            day = self.execution_date.day
+
+            s3_path = f"s3://{self.s3_bucket}/{self.s3_key}/{year}/{month}/{day}/{self.s3_key}"
+
+
+        formatted_sql_timeformat = StageTablesToRedshiftOperator.copy_sql.format(
             self.table,
             s3_path,
             credentials.access_key,
             credentials.secret_key,
-            self.json,
-            self.time_format
+            self.region,
+            self.file_format
         )
 
-        formatted_sql = StageTablesToRedshiftOperator.copy_sql.format(
-            self.table,
-            s3_path,
-            credentials.access_key,
-            credentials.secret_key,
-            self.json,
-        )
-
-        self.log.info(f"loading data into staging table: {self.table}")
-
-        if self.time_format:
-            redshift.run(formatted_sql)
-        else:
-            redshift.run(formatted_sql_timeformat)
+        redshift.run(formatted_sql_timeformat)
+        self.log.info(f"Data Loaded data into {self.table}")
