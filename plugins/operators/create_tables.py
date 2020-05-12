8167847bc04aea33dfd7676e73e57cd60d639077
theKoladeAkande <akandekolade@gmail.com>
2020-05-12 11:42:25 +0000

    feat: Opearator to create tables from queries on a file
    Operator loads a .sql file process it to filter out queries
    to create tables in redshift.

--- plugins/operators/create_tables.py
+++ plugins/operators/create_tables.py
@@ -1,33 +1,43 @@
+import pathlib
+
 from airflow.hooks.postgres_hook import PostgresHook
 from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
 
+FILE = pathlib.Path(__file__).resolve().parent /  'create_tables.sql'
+
 class CreateTableOperator(BaseOperator):
-    create_table_file='create_tables.sql'
 
 
     @apply_defaults
     def  __init__(self,
                 redshift_conn_id="",
-                  file=None,
+                tables=None,
                   *args,
                   **kwargs):
         super(CreateTableOperator, self).__init__(*args, **kwargs)
         self.redshift_conn_id = redshift_conn_id
-
+        self.tables = tables
 
     def execute(self, context):
         redshift = PostgresHook(self.redshift_conn_id)
         #load sql file
-        with open(CreateTableOperator.create_table_file, 'r') as f:
+        with open(FILE, 'r') as f:
             sql_reads = f.read().strip().split(';')
+            print('done')
 
         #process sql file
         sql_statements = [ query.replace('\n\t', "").replace('\n', "")\
                          for query in sql_reads ]
 
-        self.log.info("Creating tables in redshift")
+
+        for table in self.tables:
+            redshift.run(f"DROP TABLE IF EXISTS {table}")
+            self.log.info("Dropped {table} table Successfully")
+
         for query in sql_statements:
             if not query == "":
                 self.log.info("query: "+query)
                 redshift.run(query, autocommit=True)
+        self.log.info('Created tables successfully')
+
