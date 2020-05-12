a309bad30ca884a56a2311a22f56a562d23ed783
theKoladeAkande <akandekolade@gmail.com>
2020-05-12 11:34:00 +0000

    feat: Operator to perform data quality checks
    Performs data quality check verifying that data is actually loaded into
    the database.

---
+++ plugins/operators/data_quality.py
@@ -0,0 +1,25 @@
+from airflow.hooks.postgres_hook import PostgresHook
+from airflow.models import BaseOperator
+from airflow.utils.decorators import apply_defaults
+
+
+class DataQualityOperator(BaseOperator):
+
+    @apply_defaults
+    def __init__(self,
+                 redshift_conn_id="",
+                 tables=None,
+                 *args, **kwargs):
+        super(DataQualityOperator, self).__init__(*args, **kwargs)
+        self.redshift_conn_id = redshift_conn_id
+        self.tables = tables
+
+    def execute(self, context):
+        redshift_hook = PostgresHook(postgres_conn_id=self.redshift_conn_id)
+        for table in self.tables:
+            records = redshift_hook.get_records(f"SELECT COUNT(*) FROM {table}")
+            if len(records) < 1 or len(records[0]) < 1 or records[0][0] < 1:
+                self.log.error(f"Error! {table} table has no record")
+                raise ValueError(f"Error! {table} table has no records ")
+            self.log.info(f"Record checks passed {table} table has  {records[0][0]} records")
+            pass
